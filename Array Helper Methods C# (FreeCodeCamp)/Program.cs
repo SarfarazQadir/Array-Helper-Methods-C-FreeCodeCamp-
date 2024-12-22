932ac0b0824f8ea3503b7ea02e02afbeb1ef0b89
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-12-22 07:31:44 +0500

    Resize

--- Array Helper Methods C# (FreeCodeCamp)/Program.cs
+++ Array Helper Methods C# (FreeCodeCamp)/Program.cs
@@ -1,18 +1,91 @@
-﻿Console.WriteLine("Array Helper Methods");
+﻿//Console.WriteLine("Array Helper Methods");
+
+//string[] pallets = ["B14", "A11", "B12", "A13"];
+
+//Console.WriteLine("Sorted...");
+//Array.Sort(pallets);
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+//Console.WriteLine("");
+//Console.WriteLine("Reversed...");
+//Array.Reverse(pallets);
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+//Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"Clearing 2 counts... : {pallets.Length} ");
+//foreach (var item in pallets)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+//string[] pallets = ["B14", "A11", "B12", "A13"];
+//Console.WriteLine("");
+
+//foreach (var item in pallets)
+//{
+
+//Console.WriteLine(item);
+//}
+
+//Console.WriteLine($"Before: {pallets[0]}");
+//Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"After: {pallets[0]}");
+
+//Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+//foreach (var pallet in pallets)
+//{
+//    Console.WriteLine($"-- {pallet}");
+//}
+
+
+
+//string[] pallets = ["B14", "A11", "B12", "A13"];
+//Console.WriteLine("");
+
+//Console.WriteLine($"Before : {pallets[0].ToLower()}");
+//Array.Clear(pallets, 0, 2);
+//Console.WriteLine($"After : {pallets[0].ToLower()}");
+
+
+//  Array Resize Method
 
 string[] pallets = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
 
 Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Reverse(pallets);
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.ReadKey();
